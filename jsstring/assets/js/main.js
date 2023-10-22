729eca3cf22fa9a1294bffeead46af434bda80da
Leylailyas <sd5m2zz3l@code.edu.az>
2023-10-22 17:53:29 +0400

    first commit

---
+++ jsstring/assets/js/main.js
@@ -0,0 +1,122 @@
+1.
+
+// let root1, root2;
+// let a = 1;
+// let b = -8;
+// let c = 9;
+// let discriminant = b * b - 4 * a * c;
+// if (discriminant > 0) {
+//     let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+//     let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+//     console.log("İki heqiqi koku var:");
+//     console.log("Kök 1: " + root1);
+//     console.log("Kök 2: " + root2);
+// } else if (discriminant === 0) {
+//     let root = -b / (2 * a);
+//     console.log("Tek bir heqiqi koku var:");
+//     console.log("Kök: " + root);
+// } else {
+//     console.log("Heqiqi kök yoxdu");
+// }
+
+// 2.
+
+// let a= 11;
+// for (let i = 1; i<=1000; i++) {
+//     if (i % a === 0) {
+//         console.log(i);
+//     }
+// }
+
+// 3.
+// const arr = [2, 4, 5, 9, 1];
+// let minelement = Math.min(...arr);
+// console.log("Array-in minimum elementi:",minelement)
+
+// 4.
+// const arr = [2, 4, 5, 9, 1];
+// let maxelement = Math.max(...arr);
+// console.log("Array-in maximum elementi;",maxelement)
+ 
+
+// 5. 
+// const arr = [2, 4, 5, 9, 1];
+// const min = Math.min(...arr);
+// const index = arr.indexOf(min);
+// console.log("arrayin min element indexi:",index);
+
+// 6.
+
+// const arr = [2, 4, 5, 9, 1];
+// const max = Math.max(...arr);
+// const index = arr.indexOf(max);
+// console.log("arrayin max element indexi:",index);
+
+// 7.
+// const arr = [2, 4, 5, 9, 1];
+// let sum = 0;
+// for (let i = 1; i < arr.length; i+=2) {
+//     sum +=arr[i];
+
+// }
+// console.log("cem:",sum);
+
+
+// 8.
+// let text = "Men Code Academy telebesiyem.";
+// let result = text.toLocaleLowerCase();
+// console.log(result);
+
+
+// 9.
+// let text = "            Mən Code Academydə Programing tədrisi alıram            "
+// let  result = text.trim();
+// console.log(result);
+
+
+10.
+
+// const cumle = "Men Code Academy telebesiyem";
+// const sozler = cumle.split(" ");
+// console.log(sozler);
+
+11.
+
+let text = "            Mən Code Academydə Programing tədrisi alıram            " 
+console.log(text.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 10.
+// const cumle = "Men Code Academy Telebesiyem";
+// const sozler = cumle.split(" ");
+// console.log(sozler);
